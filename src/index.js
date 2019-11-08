a330ce17c0197a388e234f000ee35b9dc2a5a94a
katypaiva <katyfeitozaa@gmail.com>
2019-11-08 10:23:40 -0300

    refatorando o src/index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ const clickTwo = document.querySelector(".decode-btn");
 
 clickOne.addEventListener("click", (event) => {
     event.preventDefault();
-    let encodeValue= document.querySelector(".encode").value;
-    let encodeKey = parseInt(document.querySelector(".key").value);
-    let encodeResult = window.cipher.encode(encodeKey, encodeValue);
+    const encodeValue= document.querySelector(".encode").value;
+    const encodeKey = parseInt(document.querySelector(".key").value);
+    const encodeResult = window.cipher.encode(encodeKey, encodeValue);
     document.querySelector('.first-mensenger').innerHTML = encodeResult;
 });
 
 clickTwo.addEventListener("click", (event) => {
     event.preventDefault();
-    let decodeValue = document.querySelector(".decode").value;
-    let decodeKey = parseInt(document.querySelector(".key-two").value);
-    let decodeResult = window.cipher.decode(decodeValue, decodeKey);
+    const decodeValue = document.querySelector(".decode").value;
+    const decodeKey = parseInt(document.querySelector(".key-two").value);
+    const decodeResult = window.cipher.decode(decodeValue, decodeKey);
     document.querySelector('.second-mensenger').innerHTML = decodeResult;
 });
