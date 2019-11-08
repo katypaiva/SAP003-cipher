7e81ef2122803991c514bd3ef2c1e5f140ef58d6
katypaiva <katyfeitozaa@gmail.com>
2019-11-08 10:20:10 -0300

    refatorando o src/cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -6,26 +6,26 @@ window.cipher = {
 function encode(offset, str) {
     let result = "";
     for (let i = 0; i < str.length; i++) {
-        if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-            result += String.fromCharCode(((str.charCodeAt(i) - 65) + (offset % 26+26)) % 26 + 65);
-        } else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
-            result += String.fromCharCode(((str.charCodeAt(i) - 97) + (offset % 26+26)) % 26 + 97);
+        const stringAscii = str.charCodeAt(i);
+        if (stringAscii >= 65 && stringAscii <= 90) {
+            result += String.fromCharCode(((stringAscii - 65) + (offset % 26+26)) % 26 + 65);
+        } else if (stringAscii >= 97 && stringAscii <= 122) {
+            result += String.fromCharCode(((stringAscii - 97) + (offset % 26+26)) % 26 + 97);
         } else{
             result += str[i];
         }
     }
     return result;
-     
 }
 
-    
 function decode(str, offset) {
     let resultTwo = "";
     for (let i = 0; i < str.length; i++) {
-        if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-         resultTwo += String.fromCharCode(((str.charCodeAt(i) - 90) - (offset % 26+26)) % 26 + 90);
-        } else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
-            resultTwo += String.fromCharCode(((str.charCodeAt(i) - 122) - (offset % 26+26)) % 26 + 122);
+        const stringAscii = str.charCodeAt(i);
+        if (stringAscii >= 65 && stringAscii <= 90) {
+         resultTwo += String.fromCharCode(((stringAscii - 90) - (offset % 26+26)) % 26 + 90);
+        } else if (stringAscii >= 97 && stringAscii <= 122) {
+            resultTwo += String.fromCharCode(((stringAscii - 122) - (offset % 26+26)) % 26 + 122);
         } else {
             resultTwo += str[i];
         }
